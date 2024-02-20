2de9bcfb6dc2c3b62b5fed76f11ca889afc94441
handrespatrick <handrespatrick@gmail.com>
2024-02-20 18:22:39 -0300

    feat: implementing auth register controller

---
+++ src/presentation/controllers/auth/auth-register-controller.ts
@@ -0,0 +1,24 @@
+import { IAuthRegisterUseCase } from "../../../domain/usecases/auth";
+import { ok, serverError, conflict } from "../../helpers/http-helper";
+import { IController } from "../../protocols/controller-protocol";
+
+export class AuthRegisterController implements IController {
+  constructor(private readonly authRegisterUsecase: IAuthRegisterUseCase) {}
+
+  public async handle(
+    request: IController.Params
+  ): Promise<IController.Result> {
+    try {
+      const params = request.body as IAuthRegisterUseCase.Params;
+
+      const result = await this.authRegisterUsecase.authRegister(params);
+      if (result === "E-mail already in use") {
+        return conflict(result);
+      }
+
+      return ok(result);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
