71d64df0c5b136a512b16fb18e0cc1624b7672f9
handrespatrick <handrespatrick@gmail.com>
2024-02-20 18:18:30 -0300

    feat: implementing update category service

---
+++ src/application/services/categories/index.ts
@@ -0,0 +1,5 @@
+export * from "./create-category-service";
+export * from "./delete-category-service";
+export * from "./find-all-categories-service";
+export * from "./find-by-id-category-service";
+export * from "./update-category-service";
---
+++ src/application/services/categories/update-category-service.ts
@@ -0,0 +1,31 @@
+import { IUpdateCategoryUseCase } from "../../../domain/usecases/categories/update-category-usecase";
+import { IUpdateCategoryRepository } from "../../protocols/db/categories/update-category-repository-protocol";
+
+export class UpdateCategoryUseCase implements IUpdateCategoryUseCase {
+  constructor(
+    private readonly _categoryRepository: IUpdateCategoryRepository
+  ) {}
+
+  async update(
+    data: IUpdateCategoryUseCase.Params
+  ): Promise<IUpdateCategoryUseCase.Result> {
+    const { id } = data;
+
+    const hasCategory = await this._categoryRepository.findById(id);
+    if (!hasCategory) {
+      return {
+        type: "error",
+        message: `Category ${id} not found`,
+      };
+    }
+
+    const categoryUpdated = await this._categoryRepository.update(data);
+
+    return {
+      id: categoryUpdated.categoria_id,
+      name: categoryUpdated.nome_categoria,
+      description: categoryUpdated.descricao_categoria,
+      updatedAt: new Date(),
+    };
+  }
+}
