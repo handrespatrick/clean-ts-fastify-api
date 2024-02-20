715617bf9b04c712bb9bdd5a5fbf6ef488c3ea77
handrespatrick <handrespatrick@gmail.com>
2024-02-20 18:20:11 -0300

    feat: implementing update category repository protocol

---
+++ src/application/protocols/db/categories/index.ts
@@ -0,0 +1,5 @@
+export * from "./create-category-repository-protocol";
+export * from "./delete-category-repository-protocol";
+export * from "./find-all-categories-repository-protocol";
+export * from "./find-by-id-category-repository-protocol";
+export * from "./update-category-repository-protocol";
---
+++ src/application/protocols/db/categories/update-category-repository-protocol.ts
@@ -0,0 +1,21 @@
+import { IFindByIdCategoryRepository } from "./find-by-id-category-repository-protocol";
+
+export interface IUpdateCategoryRepository {
+  findById(id: number): Promise<IFindByIdCategoryRepository.Result>;
+  update(
+    data: IUpdateCategoryRepository.Params
+  ): Promise<IUpdateCategoryRepository.Result>;
+}
+
+export namespace IUpdateCategoryRepository {
+  export type Params = {
+    id: number;
+    name: string | null;
+    description: string | null;
+  };
+  export type Result = {
+    categoria_id: number;
+    nome_categoria: string | null;
+    descricao_categoria: string | null;
+  };
+}
