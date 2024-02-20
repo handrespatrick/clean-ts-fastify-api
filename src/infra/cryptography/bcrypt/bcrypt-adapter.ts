0c8eeced5a9771795fa455ae4d9249304697a00e
handrespatrick <handrespatrick@gmail.com>
2024-02-20 18:21:57 -0300

    feat: implementing bcrypt adapter

---
+++ src/infra/cryptography/bcrypt/bcrypt-adapter.ts
@@ -0,0 +1,17 @@
+import {
+  IBcryptHashComparer,
+  IBcryptHasher,
+} from "../../../application/protocols/cryptography/bcrypt/bcrypt-protocol";
+import bcrypt from "bcrypt";
+
+export class BcryptAdapter implements IBcryptHasher, IBcryptHashComparer {
+  constructor(private readonly salt: number) {}
+
+  async hash(plaintext: string): Promise<string> {
+    return bcrypt.hash(plaintext, this.salt);
+  }
+
+  async compare(plaintext: string, digest: string): Promise<boolean> {
+    return bcrypt.compare(plaintext, digest);
+  }
+}
