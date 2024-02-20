947e973d34cb0750232a10050fdc2a352a9f2c20
handrespatrick <handrespatrick@gmail.com>
2024-02-20 18:21:42 -0300

    feat: implementing auth respository

---
+++ src/infra/db/auth/auth-repository.ts
@@ -0,0 +1,33 @@
+import {
+  IAuthLoginRepository,
+  IAuthRegisterRepository,
+} from "../../../application/protocols/db/auth";
+import { prismaClient } from "../prisma-client";
+
+export class AuthRepository
+  implements IAuthLoginRepository, IAuthRegisterRepository
+{
+  async findUserByEmail(email: string): Promise<IAuthLoginRepository.Result> {
+    return prismaClient.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+
+  async addUser(
+    userData: IAuthRegisterRepository.Params
+  ): Promise<IAuthRegisterRepository.Result> {
+    return prismaClient.user.create({
+      data: {
+        email: userData.email,
+        username: null,
+        senha: userData.password,
+        nome: null,
+        cpf: "",
+        telefone: null,
+        data_nascimento: null,
+      },
+    });
+  }
+}
