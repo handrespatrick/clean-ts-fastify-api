25346dd16f23cd0f42546718da07ef3dd8b1914f
handrespatrick <handrespatrick@gmail.com>
2024-02-20 18:19:28 -0300

    feat: implementing create category repository protocol

---
+++ src/application/protocols/db/categories/create-category-repository-protocol.ts
@@ -0,0 +1,17 @@
+export interface ICreateCategoryRepository {
+  create(
+    data: ICreateCategoryRepository.CreateParams
+  ): Promise<ICreateCategoryRepository.CreateResult>;
+}
+
+export namespace ICreateCategoryRepository {
+  export type CreateParams = {
+    name: string;
+    description: string;
+  };
+  export type CreateResult = {
+    categoria_id: number;
+    nome_categoria: string | null;
+    descricao_categoria: string | null;
+  };
+}
